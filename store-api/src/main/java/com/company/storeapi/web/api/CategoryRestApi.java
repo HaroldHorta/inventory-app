30a2332fc834e4704400ed528066b336e8fa8c11
hhortasu <harolds1010@gmail.com>
2020-10-20 12:01:38 -0500

    Se agrega el servico para actuaalizar la categoria

--- store-api/src/main/java/com/company/storeapi/web/api/CategoryRestApi.java
+++ store-api/src/main/java/com/company/storeapi/web/api/CategoryRestApi.java
@@ -2,17 +2,21 @@ package com.company.storeapi.web.api;
 
 import com.company.storeapi.core.exceptions.base.ServiceException;
 import com.company.storeapi.model.payload.request.category.RequestAddCategoryDTO;
+import com.company.storeapi.model.payload.request.customer.RequestUpdateCustomerDTO;
 import com.company.storeapi.model.payload.response.category.ResponseCategoryDTO;
+import com.company.storeapi.model.payload.response.customer.ResponseCustomerDTO;
 import com.company.storeapi.services.category.CategoryService;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+/**
+ * The type Category rest api.
+ */
 @RestController
 @RequestMapping(value = "/api/category")
 @CrossOrigin({"*"})
@@ -20,34 +24,77 @@ public class CategoryRestApi {
 
     private final CategoryService service;
 
+    /**
+     * Instantiates a new Category rest api.
+     *
+     * @param service the service
+     */
     public CategoryRestApi(CategoryService service) {
         this.service = service;
     }
 
 
+    /**
+     * Gets all category.
+     *
+     * @return the all category
+     * @throws ServiceException the service exception
+     */
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-    //@PreAuthorize("hasRole('SELLER')")
     public List<ResponseCategoryDTO> getAllCategory() throws ServiceException {
         return service.getAllCategory();
     }
 
-    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<ResponseCategoryDTO> create(@RequestBody RequestAddCategoryDTO requestAddCategoryDTO) throws ServiceException {
-        ResponseCategoryDTO created = service.saveCategory(requestAddCategoryDTO);
-        return new ResponseEntity<>(created, new HttpHeaders(), HttpStatus.OK);
-    }
-
+    /**
+     * Gets category by id.
+     *
+     * @param id the id
+     * @return the category by id
+     * @throws ServiceException the service exception
+     */
     @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
-   // @PreAuthorize("hasRole('ADMINISTRATOR')")
     public ResponseEntity<ResponseCategoryDTO> getCategoryById(@PathVariable("id") String id)
             throws ServiceException {
         ResponseCategoryDTO entity = service.validateAndGetCategoryById(id);
         return new ResponseEntity<>(entity, new HttpHeaders(), HttpStatus.OK);
     }
 
-   @DeleteMapping(value = "/{id}")
-    public void delete(@PathVariable String id) throws ServiceException{
-      service.deleteById(id);
+    /**
+     * Create response entity.
+     *
+     * @param requestAddCategoryDTO the request add category dto
+     * @return the response entity
+     * @throws ServiceException the service exception
+     */
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<ResponseCategoryDTO> create(@RequestBody RequestAddCategoryDTO requestAddCategoryDTO) throws ServiceException {
+        ResponseCategoryDTO created = service.saveCategory(requestAddCategoryDTO);
+        return new ResponseEntity<>(created, new HttpHeaders(), HttpStatus.OK);
+    }
+
+    /**
+     * Update response entity.
+     *
+     * @param id                    the id
+     * @param requestAddCategoryDTO the request add category dto
+     * @return the response entity
+     * @throws ServiceException the service exception
+     */
+    @PutMapping(value = "/{id}", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<ResponseCategoryDTO> update (@PathVariable String id, @RequestBody RequestAddCategoryDTO requestAddCategoryDTO) throws ServiceException{
+        ResponseCategoryDTO update = service.updateCategory(id, requestAddCategoryDTO);
+        return new ResponseEntity<>(update, new HttpHeaders(), HttpStatus.OK);
+    }
+
+    /**
+     * Delete.
+     *
+     * @param id the id
+     * @throws ServiceException the service exception
+     */
+    @DeleteMapping(value = "/{id}")
+    public void delete(@PathVariable String id) throws ServiceException {
+        service.deleteById(id);
     }
 
 }
