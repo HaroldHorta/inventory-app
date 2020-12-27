827cd49ab0a38138be8df3a23c83937f512a5d72
Harold <hahorta@misena.edu.co>
2020-12-26 21:27:42 -0500

    correcciones ciclos

--- store-api/src/main/java/com/company/storeapi/services/category/impl/CategoryServiceImpl.java
+++ store-api/src/main/java/com/company/storeapi/services/category/impl/CategoryServiceImpl.java
@@ -1,22 +1,18 @@
 package com.company.storeapi.services.category.impl;
 
-import com.company.storeapi.core.exceptions.base.ServiceException;
 import com.company.storeapi.core.exceptions.enums.LogRefServices;
 import com.company.storeapi.core.exceptions.persistence.DataNotFoundPersistenceException;
 import com.company.storeapi.core.mapper.CategoryMapper;
 import com.company.storeapi.core.mapper.ProductMapper;
+import com.company.storeapi.model.entity.Category;
 import com.company.storeapi.model.entity.Order;
 import com.company.storeapi.model.entity.Product;
-import com.company.storeapi.model.entity.Ticket;
 import com.company.storeapi.model.payload.request.category.RequestAddCategoryDTO;
 import com.company.storeapi.model.payload.request.category.RequestUpdateCategoryDTO;
 import com.company.storeapi.model.payload.response.category.ResponseCategoryDTO;
-import com.company.storeapi.model.entity.Category;
-import com.company.storeapi.model.payload.response.product.ResponseOrderProductItemsDTO;
 import com.company.storeapi.repositories.category.facade.CategoryRepositoryFacade;
 import com.company.storeapi.repositories.order.facade.OrderRepositoryFacade;
 import com.company.storeapi.repositories.product.facade.ProductRepositoryFacade;
-import com.company.storeapi.repositories.tickey.facade.TicketRepositoryFacade;
 import com.company.storeapi.services.category.CategoryService;
 import com.company.storeapi.services.product.impl.ProductServiceImpl;
 import lombok.RequiredArgsConstructor;
@@ -33,14 +29,13 @@ public class CategoryServiceImpl implements CategoryService {
     private final CategoryRepositoryFacade repositoryFacade;
     private final ProductRepositoryFacade productRepositoryFacade;
     private final OrderRepositoryFacade orderRepositoryFacade;
-    private final TicketRepositoryFacade ticketRepositoryFacade;
 
     private final CategoryMapper categoryMapper;
     private final ProductMapper productMapper;
 
 
     @Override
-    public List<ResponseCategoryDTO> getAllCategory() throws ServiceException {
+    public List<ResponseCategoryDTO> getAllCategory() {
         List<Category> categoryList = repositoryFacade.getAllCategory();
         return categoryList.stream().map(categoryMapper::toCategoryDto).collect(Collectors.toList());
     }
@@ -51,24 +46,23 @@ public class CategoryServiceImpl implements CategoryService {
     }
 
     @Override
-    public ResponseCategoryDTO saveCategory(RequestAddCategoryDTO requestAddCategoryDTO) throws ServiceException {
+    public ResponseCategoryDTO saveCategory(RequestAddCategoryDTO requestAddCategoryDTO) {
         return categoryMapper.toCategoryDto(repositoryFacade.saveCategory(categoryMapper.toCategory(requestAddCategoryDTO)));
     }
 
     @Override
-    public ResponseCategoryDTO updateCategory(RequestUpdateCategoryDTO requestUpdateCategoryDTO) throws ServiceException {
+    public ResponseCategoryDTO updateCategory(RequestUpdateCategoryDTO requestUpdateCategoryDTO) {
         Category category = repositoryFacade.validateAndGetCategoryById(requestUpdateCategoryDTO.getId());
         List<Product> productList = productRepositoryFacade.findProductByCategory(requestUpdateCategoryDTO.getId());
 
         for (Product p : productList) {
             Product product = productRepositoryFacade.validateAndGetProductById(p.getId());
             LinkedHashSet<ResponseCategoryDTO> listCategory = new LinkedHashSet<>();
-            for (ResponseCategoryDTO ignored : product.getCategory()) {
-                ResponseCategoryDTO cat = new ResponseCategoryDTO();
+            product.getCategory().stream().map(ignored -> new ResponseCategoryDTO()).forEach(cat -> {
                 cat.setId(category.getId());
                 cat.setDescription(requestUpdateCategoryDTO.getDescription());
                 listCategory.add(cat);
-            }
+            });
             product.setCategory(listCategory);
             productRepositoryFacade.saveProduct(product);
 
@@ -84,7 +78,7 @@ public class CategoryServiceImpl implements CategoryService {
     }
 
     @Override
-    public void deleteById(String id) throws ServiceException {
+    public void deleteById(String id) {
         List<Product> productList = productRepositoryFacade.findProductByCategory(id);
         if (productList.isEmpty()) {
             repositoryFacade.deleteCategory(id);
