b52238640fd04adfdbecf58628e1d044361e053a
hhortasu <harolds1010@gmail.com>
2021-01-04 10:04:06 -0500

    Se agrega el estado de la paginacion de categoria

--- store-api/src/main/java/com/company/storeapi/model/payload/response/category/ResponseListCategoryPaginationDto.java
+++ store-api/src/main/java/com/company/storeapi/model/payload/response/category/ResponseListCategoryPaginationDto.java
@@ -8,5 +8,5 @@ import java.util.List;
 public class ResponseListCategoryPaginationDto {
 
     private int count;
-    private List<ResponseCategoryDTO> customers;
+    private List<ResponseCategoryDTO> categories;
 }
