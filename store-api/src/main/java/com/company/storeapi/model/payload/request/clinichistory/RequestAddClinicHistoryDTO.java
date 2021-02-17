52f3573f6d1d58e7e36f423c8f62d2156b32f096
Harold <harolds1010@gmail.com>
2021-02-17 11:59:59 -0500

    mascotas

--- store-api/src/main/java/com/company/storeapi/model/payload/request/clinichistory/RequestAddClinicHistoryDTO.java
+++ store-api/src/main/java/com/company/storeapi/model/payload/request/clinichistory/RequestAddClinicHistoryDTO.java
@@ -3,13 +3,10 @@ package com.company.storeapi.model.payload.request.clinichistory;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
-import java.util.Date;
 
 @Data
 public class RequestAddClinicHistoryDTO {
 
-    private Date createAt;
-    @Schema(example = "601ddd6e1f971d29dcec53de")
     private String veterinary;
     @Schema(example = "601c835df4b9c71f38e3946e")
     private String customer;
--- store-api/src/main/java/com/company/storeapi/model/payload/response/vaccination/ResponseVaccinationDTO.java
+++ store-api/src/main/java/com/company/storeapi/model/payload/response/vaccination/ResponseVaccinationDTO.java
@@ -3,13 +3,15 @@ package com.company.storeapi.model.payload.response.vaccination;
 import com.company.storeapi.model.enums.Status;
 import lombok.Data;
 
+import java.util.Date;
+
 @Data
 public class ResponseVaccinationDTO {
 
     private String id;
     private String description;
     private Status status;
-    private String createAt;
+    private Date createAt;
     private String lot;
     private String observation;
 
